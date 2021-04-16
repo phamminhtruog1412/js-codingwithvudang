17940e5bf9cbfc58f997e118794757b5178e8bb0
Pmt <phamminhtruog1412@gmail.com>
2021-04-16 12:28:10 +0700

    add more detail

--- 01_varriables/app.js
+++ 01_varriables/app.js
@@ -2,11 +2,14 @@
  * global scope
  */
 
-
+var x = 100
 function sayLoveNhu(){ //function scope
     for(var i= 0; i<10 ; i++){ //block scope
         console.log(i)
+
     }
+    var z="ongtrumbackend"
+    console.log(z)
 }
 sayLoveNhu()
-
+console.log(x)
