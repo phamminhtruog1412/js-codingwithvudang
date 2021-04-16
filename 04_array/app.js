05f8df18b0caf154147d2abccc0aedbb3909a29b
Pmt <phamminhtruog1412@gmail.com>
2021-04-16 13:24:06 +0700

    add array

---
+++ 04_array/app.js
@@ -0,0 +1,11 @@
+//Array khai bao 
+var pmt =["Nhu", "Phuong", "Thy", "Nhi"]
+
+pmt.push("Van")
+console.log(pmt)
+
+//Array.for.each
+var number=[0, 9, 7, 1, 5, 1, 2, 7, 4, 9]
+number.forEach(x => console.log(x))
+
+//Array map, filter
