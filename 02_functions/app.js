aa37d0b5b6fff9d8e7e8dd31a0fd8f209b9f956b
Pmt <phamminhtruog1412@gmail.com>
2021-04-16 12:48:31 +0700

    add 02

---
+++ 02_functions/app.js
@@ -0,0 +1,16 @@
+/**
+ * Function declaration
+ */
+function sayLove(name, age){
+    console.log(`my name is ${name} and i'm ${age}` )
+  
+}
+sayLove("Nhu", 23)
+
+/**
+ * Varriable declaration & Expression declaration
+ */
+const func = function(love){
+    console.log(`Love Nhu ${love} lan`)
+}
+func (3000)
