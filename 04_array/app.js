4eafe8962cbef620a314c74a0f1181848c93b986
Pmt <phamminhtruog1412@gmail.com>
2021-04-16 13:54:55 +0700

    add filter, map

--- 04_array/app.js
+++ 04_array/app.js
@@ -1,11 +1,18 @@
 //Array khai bao 
-var pmt =["Nhu", "Phuong", "Thy", "Nhi"]
+//var pmt =["Nhu", "Phuong", "Thy", "Nhi"]
 
-pmt.push("Van")
-console.log(pmt)
+//pmt.push("Van")
+//console.log(pmt)
 
 //Array.for.each
-var number=[0, 9, 7, 1, 5, 1, 2, 7, 4, 9]
-number.forEach(x => console.log(x))
+//var number=[0, 9, 7, 1, 5, 1, 2, 7, 4, 9]
+//number.forEach(x => console.log(x))
 
-//Array map, filter
+//Array map
+var number=[1,2,3,4,5,6,7,7,8,9]
+number.map(x => console.log(x*3))
+
+// filter
+var number=[2,3,4,6,7,8,1]
+var t = number.filter(x => x >5)
+console.log(t)
